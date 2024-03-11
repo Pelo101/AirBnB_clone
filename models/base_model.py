1dca0c76ea08271448be4c958ac8ef6572d30965
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-03-11 07:34:34 -0700

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,7 @@ class BaseModel:
 
     def __str__(self):
         """Returns string representation of the class"""
-        return f"self.__class__.__name__({self.id}){self.__dict__}"
+        return f"self.__class__.__name__,self.id,self.__dict__"
 
     def save(self):
         """Updates the public instance attribute updated_at"""
